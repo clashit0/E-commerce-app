eaed17a23650c14404630121a1e50e98f04ac9e0
Abhinav jain <jainabhinav800@gmail.com>
2025-11-13 12:55:06 +0530

    intial setup for EachProductDetailScreen.kt file

--- app/src/main/java/com/abhinav/shoppingapp/presentation/screens/EachProductDetailScreen.kt
+++ app/src/main/java/com/abhinav/shoppingapp/presentation/screens/EachProductDetailScreen.kt
@@ -1,12 +1,282 @@
 package com.abhinav.shoppingapp.presentation.screens
 
+import android.R
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FilterChip
+import androidx.compose.material3.FilterChipDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavController
+import coil.compose.AsyncImage
+import com.abhinav.shoppingapp.domain.models.CartDataModels
 import com.abhinav.shoppingapp.presentation.navigation.Routes
 import com.abhinav.shoppingapp.presentation.viewModels.ShoppingAppViewModel
 
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalMaterialApi::class)
 @Composable
-fun EachProductDetailsScreen(navController: NavController, productId: String){
+fun EachProductDetailsScreen(
+    navController: NavController,
+    productId: String,
+    viewModel: ShoppingAppViewModel = hiltViewModel()
+) {
+
+    val getProductById = viewModel.getProductByIdState.collectAsStateWithLifecycle()
+
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior(rememberTopAppBarState())
+
+    val context = LocalContext.current
+
+    var selectedSize by remember { mutableStateOf("") }
+    var quantity by remember { mutableIntStateOf(1) }
+    val isFavourite by remember { mutableStateOf(false) }
+
+
+    LaunchedEffect(key1 = Unit) {
+        viewModel.getProductById(productId)
+    }
+
+    Scaffold(
+        modifier = Modifier
+            .fillMaxSize()
+            .nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            TopAppBar(
+                title = { Text("Product Details") },
+                navigationIcon = {
+                    IconButton(onClick = { navController.popBackStack() }) {
+                        Icon(
+                            Icons.AutoMirrored.Default.ArrowBack,
+                            "Back"
+                        )
+                    }
+                },
+                scrollBehavior = scrollBehavior
+            )
+        }
+
+    ) { innerPadding ->
+
+
+        when {
+            getProductById.value.isLoading -> {
+                Box(
+                    modifier = Modifier.fillMaxSize(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    CircularProgressIndicator()
+                }
+            }
+
+            getProductById.value.errorMessage != null -> {
+                Box(
+                    modifier = Modifier.fillMaxSize(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Spacer(Modifier.padding(8.dp))
+                    Text(text = getProductById.value.errorMessage.toString())
+                }
+            }
+
+            getProductById.value.userData != null -> {
+                val product = getProductById.value.userData!!.copy(productId = productId)
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(innerPadding)
+                        .verticalScroll(rememberScrollState())
+                ) {
+                    Box(modifier = Modifier.height(300.dp)) {
+                        AsyncImage(
+                            model = product.image,
+                            contentDescription = null,
+                            modifier = Modifier.fillMaxSize(),
+                            contentScale = ContentScale.Crop
+                        )
+                    }
+
+                    Column(
+                        modifier = Modifier.padding(16.dp)
+                    ) {
+                        Text(
+                            text = product.name,
+                            style = MaterialTheme.typography.headlineMedium,
+                            fontWeight = FontWeight.Bold
+                        )
+                        Text(
+                            text = " Rs ${product.price}",
+                            style = MaterialTheme.typography.headlineSmall,
+                            color = MaterialTheme.colorScheme.primary,
+                            modifier = Modifier.padding(vertical = 8.dp)
+                        )
+                        Text(
+                            text = "Size",
+                            style = MaterialTheme.typography.labelLarge,
+                            modifier = Modifier.padding(top = 16.dp, bottom = 8.dp)
+                        )
+
+                        Row(
+                            horizontalArrangement = Arrangement.spacedBy(8.dp)
+                        ) {
+                            listOf("S", "M", "L", "XL", "XXL").forEach { size ->
+                                FilterChip(
+                                    selected = selectedSize == size,
+                                    onClick = { selectedSize = size },
+                                    label = { Text(text = size) },
+                                    colors = FilterChipDefaults.filterChipColors(
+                                        selectedContainerColor = MaterialTheme.colorScheme.primary,
+                                        selectedLabelColor = Color.White
+                                    )
+                                )
+                            }
+                        }
+                        Text(
+                            "Quantity",
+                            style = MaterialTheme.typography.labelLarge,
+                            modifier = Modifier.padding(top = 16.dp, bottom = 8.dp)
+                        )
+                        Row(
+                            verticalAlignment = Alignment.CenterVertically,
+                            horizontalArrangement = Arrangement.spacedBy(8.dp)
+                        ) {
+                            IconButton(onClick = {
+                                if (quantity > 1) {
+                                    quantity--
+                                }
+
+                            }) {
+                                Text(
+                                    "-",
+                                    style = MaterialTheme.typography.headlineSmall
+                                )
+                            }
+
+                            Text(
+                                quantity.toString(),
+                                style = MaterialTheme.typography.bodyLarge
+                            )
+
+                            IconButton(onClick = {
+                                quantity++
+
+                            }) {
+                                Text(
+                                    "+",
+                                    style = MaterialTheme.typography.headlineSmall
+                                )
+                            }
+
+
+                        }
+                        Text(
+                            "Description",
+                            style = MaterialTheme.typography.labelLarge,
+                            modifier = Modifier.padding(top = 16.dp, bottom = 8.dp)
+                        )
+                        Text(product.description)
+
+                        Button(
+                            onClick = {
+                                val cartDataModels = CartDataModels(
+                                    productId = product.productId,
+                                    image = product.image,
+                                    name = product.name,
+                                    price = product.price,
+                                    quantity = quantity.toString(),
+                                    size = selectedSize,
+                                    description = product.description,
+                                    category = product.category
+                                )
+                                viewModel.addToCart(cartDataModels)
+                            },
+                            modifier = Modifier.fillMaxWidth(),
+                            colors = ButtonDefaults.buttonColors(colorResource(id = R.color.holo_orange_dark))
+                        ) {
+                            Text("Add to cart")
+                        }
+
+                        Button(
+                            onClick = { navController.navigate(Routes.CheckoutScreen.route + "/${product.productId}") },
+                            modifier = Modifier.fillMaxWidth(),
+                            colors = ButtonDefaults.buttonColors(colorResource(id = R.color.holo_orange_dark))
+                        ) {
+                            Text("Buy now")
+                        }
+
+                        OutlinedButton(
+                            onClick = {
+                                isFavourite != isFavourite
+                                viewModel.addToFav(product)
+                            },
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(8.dp)
+                        ) {
+                            Row {
+                                Icon(
+                                    if (isFavourite){
+                                        Icons.Default.Favorite
+                                    }else{
+                                        Icons.Default.FavoriteBorder
+
+                                    },
+                                    contentDescription = "Favourite"
+                                )
+
+                                Text("Add to Wishlist")
+                            }
+                        }
+
+
+                    }
+                }
+
+            }
+        }
+
+    }
 
 }
