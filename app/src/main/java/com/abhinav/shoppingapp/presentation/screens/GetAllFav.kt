49af4bc78f6c40d1c848ef01812cb1c32b0e8352
Abhinav jain <jainabhinav800@gmail.com>
2025-11-14 10:19:57 +0530

    setup GetAllFav.kt

--- app/src/main/java/com/abhinav/shoppingapp/presentation/screens/GetAllFav.kt
+++ app/src/main/java/com/abhinav/shoppingapp/presentation/screens/GetAllFav.kt
@@ -1,18 +1,23 @@
 package com.abhinav.shoppingapp.presentation.screens
 
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.material3.Icon
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Card
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
@@ -27,7 +32,9 @@ import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavController
+import coil.compose.AsyncImage
 import com.abhinav.shoppingapp.domain.models.ProductsDataModels
+import com.abhinav.shoppingapp.presentation.navigation.Routes
 import com.abhinav.shoppingapp.presentation.viewModels.ShoppingAppViewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -65,13 +72,18 @@ fun GetAllFav(
             OutlinedTextField(
                 value = "",
                 onValueChange = {},
-                modifier = Modifier.fillMaxWidth()
+                modifier = Modifier
+                    .fillMaxWidth()
                     .padding(16.dp),
-                placeholder = {Text("Search")},
-                leadingIcon = { Icon(Icons.Default.Search,
-                    contentDescription = "Search")}
+                placeholder = { Text("Search") },
+                leadingIcon = {
+                    Icon(
+                        Icons.Default.Search,
+                        contentDescription = "Search"
+                    )
+                }
             )
-            when{
+            when {
                 getAllFav.value.isLoading -> {
                     Box(
                         modifier = Modifier.fillMaxSize(),
@@ -80,7 +92,8 @@ fun GetAllFav(
                         CircularProgressIndicator()
                     }
                 }
-                getAllFav.value.errorMessage != null ->{
+
+                getAllFav.value.errorMessage != null -> {
                     Box(
                         modifier = Modifier.fillMaxSize(),
                         contentAlignment = Alignment.Center
@@ -88,7 +101,8 @@ fun GetAllFav(
                         Text(text = getAllFav.value.errorMessage.toString())
                     }
                 }
-                getFavData.isEmpty() ->{
+
+                getFavData.isEmpty() -> {
                     Box(
                         modifier = Modifier.fillMaxSize(),
                         contentAlignment = Alignment.Center
@@ -96,13 +110,58 @@ fun GetAllFav(
                         Text("Your wishlist is empty")
                     }
                 }
-                else ->{
+
+                else -> {
                     LazyVerticalGrid(
-                        columns = GridCells.Fixed(2)
-                    ) { }
+                        columns = GridCells.Fixed(2),
+                        contentPadding = PaddingValues(16.dp),
+                        horizontalArrangement = Arrangement.spacedBy(16.dp),
+                        verticalArrangement = Arrangement.spacedBy(16.dp)
+                    ) {
+                        items(getFavData) { product ->
+                            ProductCard(product = product,
+                                onProductClick =  {
+                                    navController.navigate(Routes.EachProductDetailsScreen.route+"/${product.productId}")
+                                })
+                        }
+                    }
                 }
 
             }
         }
+    }
+}
+
+@Composable
+fun ProductCard(product: ProductsDataModels, onProductClick: () -> Unit) {
+    Card(
+        onClick = { onProductClick },
+        modifier = Modifier.fillMaxWidth()
+
+
+    ) {
+        Column {
+            AsyncImage(
+                model = product.image,
+                contentDescription = product.name,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(200.dp)
+            )
+
+            Column(Modifier.padding(8.dp)) {
+                Text(
+                    text = product.name,
+                    style = MaterialTheme.typography.bodyLarge,
+                    fontWeight = FontWeight.Bold
+                )
+                Text(
+                    text = product.name,
+                    style = MaterialTheme.typography.bodyMedium,
+                    color = MaterialTheme.colorScheme.primary
+                )
+            }
+        }
+
     }
 }
