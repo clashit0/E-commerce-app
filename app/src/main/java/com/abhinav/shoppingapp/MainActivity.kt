1ae893ee826fb12085e0564e70ff1e733735fb51
Abhinav jain <jainabhinav800@gmail.com>
2025-11-15 08:30:25 +0530

    Added splash screen

--- app/src/main/java/com/abhinav/shoppingapp/MainActivity.kt
+++ app/src/main/java/com/abhinav/shoppingapp/MainActivity.kt
@@ -2,12 +2,36 @@ package com.abhinav.shoppingapp
 
 import android.app.Activity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.text.BasicText
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.abhinav.shoppingapp.presentation.navigation.App
-import com.abhinav.shoppingapp.presentation.screens.LoginScreenUi
 import com.abhinav.shoppingapp.ui.theme.ShoppingAppTheme
 import com.google.firebase.auth.FirebaseAuth
 import com.razorpay.Checkout
@@ -28,10 +52,58 @@ class MainActivity : ComponentActivity(), PaymentResultWithDataListener {
         enableEdgeToEdge()
         setContent {
             ShoppingAppTheme {
-                App(firebaseAuth, {startPayment()})
+                MainScreen(firebaseAuth) { }
             }
         }
     }
+
+    @Composable
+    fun MainScreen(firebaseAuth: FirebaseAuth, onPayTest: () -> Unit) {
+        val showSplashScreen = remember { mutableStateOf(true) }
+
+        LaunchedEffect(Unit) {
+            Handler(Looper.getMainLooper()).postDelayed({
+                showSplashScreen.value = false
+            }, 3000)
+        }
+        if (showSplashScreen.value) {
+            SplashScreen()
+        } else {
+            App(firebaseAuth, { onPayTest })
+        }
+    }
+
+    @Composable
+    fun SplashScreen() {
+        Box(
+            Modifier
+                .fillMaxSize()
+                .background(color = Color.White),
+            contentAlignment = Alignment.Center
+        ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Image(painter = painterResource(R.drawable.splashscreen), contentDescription = "App icon",
+                    modifier = Modifier.size(300.dp))
+
+                Spacer(modifier = Modifier.height(20.dp))
+
+                BasicText(
+                    text = "Welcome to Shopease",
+                    style = MaterialTheme.typography.labelLarge.copy(
+                        color = Color.Black,
+                        fontSize = 18.sp,
+                        textAlign = TextAlign.Center,
+                        fontWeight = FontWeight.Bold
+                    )
+
+                )
+            }
+        }
+    }
+
 
     private fun startPayment() {
         /*
@@ -42,14 +114,14 @@ class MainActivity : ComponentActivity(), PaymentResultWithDataListener {
 
         try {
             val options = JSONObject()
-            options.put("name","Razorpay Corp")
-            options.put("description","Demoing Charges")
+            options.put("name", "Razorpay Corp")
+            options.put("description", "Demoing Charges")
             //You can omit the image option to fetch the image from the Dashboard
-            options.put("image","http://example.com/image/rzp.jpg")
+            options.put("image", "http://example.com/image/rzp.jpg")
             options.put("theme.color", "#3399cc");
-            options.put("currency","<currency>");
+            options.put("currency", "<currency>");
             options.put("order_id", "order_DBJOWzybf0sJbb");
-            options.put("amount","50000")//pass amount in currency subunits
+            options.put("amount", "50000")//pass amount in currency subunits
 
             val retryObj = JSONObject();
             retryObj.put("enabled", true);
@@ -57,13 +129,13 @@ class MainActivity : ComponentActivity(), PaymentResultWithDataListener {
             options.put("retry", retryObj);
 
             val prefill = JSONObject()
-            prefill.put("email","<email>")
-            prefill.put("contact","<phone>")
+            prefill.put("email", "<email>")
+            prefill.put("contact", "<phone>")
 
-            options.put("prefill",prefill)
-            co.open(activity,options)
-        }catch (e: Exception){
-            Toast.makeText(activity,"Error in payment: "+ e.message,Toast.LENGTH_LONG).show()
+            options.put("prefill", prefill)
+            co.open(activity, options)
+        } catch (e: Exception) {
+            Toast.makeText(activity, "Error in payment: " + e.message, Toast.LENGTH_LONG).show()
             e.printStackTrace()
         }
     }
